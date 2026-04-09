0f82b4c075c7734cbdd9dc868dad0d21d72dd142
vighnesh204 <borhadevighnesh@gmail.com>
2026-04-09 21:48:56 +0530

    add data.js file

---
+++ Course-Page/src/utils/data.js
@@ -0,0 +1,23 @@
+export const filterData = [
+  {
+    id: 1,
+    name: "ALL"
+  },
+  {
+    id: 2,
+    name: "Development"
+  },
+  {
+    id: 3,
+    name: "Business"
+  },
+  {
+    id: 4,
+    name: "Design"
+  },
+  {
+    id: 5,
+    name: "Lifestyle"
+  }
+];
+
