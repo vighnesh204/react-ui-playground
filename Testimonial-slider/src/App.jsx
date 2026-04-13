f7d6ee7d4cfe6fe2a36623b75aa71667d47bcb56
vighnesh204 <borhadevighnesh@gmail.com>
2026-04-13 13:18:33 +0530

    Initial setup done for anime testimonials

---
+++ Testimonial-slider/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Testimonials from './components/Testimonials'
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <h1>Anime Testimonials</h1>
+        <div></div>
+
+        <Testimonials />
+      </div>
+    </div>
+  )
+}
+
+export default App
