ccb2706fe3e0aad1f9e01611a39f7281449076eb
vighnesh204 <borhadevighnesh@gmail.com>
2026-04-13 14:15:10 +0530

    buttons removed from Card and add in Testimonial

--- Testimonial-slider/src/components/Card.jsx
+++ Testimonial-slider/src/components/Card.jsx
@@ -29,21 +29,6 @@ const Card = ({animeData}) => {
             <div className="text-violet-400 mx-auto mt-4">
                 <FaQuoteRight />
             </div>
-
-            <div className="flex gap-4 mt-5 text-3xl text-violet-400 font-bold justify-center">
-                <button className="cursor-pointer hover:text-violet-500">
-                    <FiChevronLeft />
-                </button>
-                <button className="cursor-pointer hover:text-violet-500">
-                    <FiChevronRight />
-                </button>
-            </div>
-
-            <div>
-                <button className="cursor-pointer px-6 py-2 rounded-xl bg-violet-400 text-white hover:bg-violet-500 transition-all font-semibold duration-300 mt-5">
-                    Anime
-                </button>
-            </div>
         </div>
     )
 }
--- Testimonial-slider/src/components/Testimonials.jsx
+++ Testimonial-slider/src/components/Testimonials.jsx
@@ -4,6 +4,21 @@ const Testimonials = ({ animeData }) => {
   return (
     <div>
         <Card animeData={animeData} />
+
+          <div className="flex gap-4 mt-5 text-3xl text-violet-400 font-bold justify-center">
+                <button className="cursor-pointer hover:text-violet-500">
+                    <FiChevronLeft />
+                </button>
+                <button className="cursor-pointer hover:text-violet-500">
+                    <FiChevronRight />
+                </button>
+            </div>
+
+            <div>
+                <button className="cursor-pointer px-6 py-2 rounded-xl bg-violet-400 text-white hover:bg-violet-500 transition-all font-semibold duration-300 mt-5">
+                    Anime
+                </button>
+            </div>
     </div>
   )
 }
